5e917d004d8350d2a04d80b77c72ae965739c481
Adithya31101 <adithya31101@gmail.com>
2021-10-18 21:21:22 +0530

    Major Commit: static file hosting

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const http = require("http");
 const PORT = process.env.PORT || 5000;
 
 app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
-   serveClient: false,
-   cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
-   }
+const io = require("socket.io")(server, {
+  serveClient: false,
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 let socket;
 const players = [];
 
-io.on('connection', (socketInstance) => {
-   socket = socketInstance;
-   console.log(socket.id);
-   socket.on("init", (playerInfo) => {
-     players.push(playerInfo);
-     console.log(players);
-   });
-   socket.on("disconnect", () => {
-      
-   });
+io.on("connection", (socketInstance) => {
+  socket = socketInstance;
+  console.log(socket.id);
+  socket.on("init", (playerInfo) => {
+    players.push(playerInfo);
+    console.log(players);
+  });
+  socket.on("disconnect", () => {});
 });
-
-app.use('/assets', express.static(path.resolve('dist/assets')));
+app.use(cors());
 app.use("/maps", express.static(path.resolve("maps")));
 app.use("/characters", express.static(path.resolve("characters")));
+app.use("/thumbnails", express.static(path.resolve("thumbnails")));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.status(200).sendFile(path.resolve('dist/index.html'));
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Milo!" });
 });
 
 server.listen(PORT, () => console.log("Listening on PORT " + PORT));
