102d19c18df64b395fd481711de265f253f4b163
Adithya31101 <adithya31101@gmail.com>
2021-10-22 16:33:27 +0530

    Major commit: Realitime communication

--- index.js
+++ index.js
@@ -13,18 +13,44 @@ const io = require("socket.io")(server, {
     methods: ["GET", "POST"],
   },
 });
+const players = {};
 
-let socket;
-const players = [];
-
-io.on("connection", (socketInstance) => {
-  socket = socketInstance;
+io.on("connection", (socket) => {
   console.log(socket.id);
-  socket.on("init", (playerInfo) => {
-    players.push(playerInfo);
-    console.log(players);
+
+  socket.on("init", (playerInfo, callback) => {
+    players[playerInfo.socId] = playerInfo;
+    console.log("\nInit\n---------------", players);
+    callback(players);
+    socket.broadcast.emit("new-player", playerInfo);
   });
-  socket.on("disconnect", () => {});
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    socket.broadcast.emit("remove-player", socket.id);
+    console.log("Disconnect\n---------------", players);
+  });
+
+  socket.on("player-movement", (data) => {
+    console.log(data);
+    players[data.socId].x = data.x;
+    players[data.socId].y = data.y;
+    socket.broadcast.emit("move-player", data);
+  });
+
+  socket.on("player-key-down", (data) => {
+    players[data.socId].keyDown = data.key;
+    console.log("Key Down", players[data.socId].keyDown);
+    socket.broadcast.emit("move-player", data);
+  });
+
+  socket.on("player-key-up", (data) => {
+    players[data.socId].keyDown = null;
+    players[data.socId].x = data.x;
+    players[data.socId].y = data.y;
+    console.log("Key Up", players[data.socId].keyDown);
+    socket.broadcast.emit("stop-player", data);
+  });
 });
 app.use(cors());
 app.use("/maps", express.static(path.resolve("maps")));
@@ -32,8 +58,6 @@ app.use("/characters", express.static(path.resolve("characters")));
 app.use("/thumbnails", express.static(path.resolve("thumbnails")));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Milo!" });
-});
+app.use("/", express.static("./dist"));
 
 server.listen(PORT, () => console.log("Listening on PORT " + PORT));
